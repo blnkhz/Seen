84e5db5af9e7c86f7341ed10e46436f81cd23ae5
Blanka Hooz <hooz.blanka@gmail.com>
2018-08-07 15:39:44 +0200

    add Sightings model

---
+++ backend/Seen/Models/Sighting.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Seen.Models
+{
+    public class Sighting
+    {
+		[Required]
+        public long Id { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public string Day { get; set; }
+        [Required]
+        public string SocialHandle { get; set; }
+        public string Gender { get; set; }
+        public string HairColor { get; set; }
+        public string HairStyle { get; set; }
+        public bool Glasses { get; set; }
+        public string Message { get; set; }
+    }
+}
