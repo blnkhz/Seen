90de9208505b800749af5178f5b9cc8e936da1d1
dugasmate <dugasmate@gmail.com>
2018-08-07 16:18:39 +0200

    add repository layer

--- backend/Seen/Models/Sighting.cs
+++ backend/Seen/Models/Sighting.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace Seen.Models
@@ -6,16 +8,26 @@ namespace Seen.Models
     public class Sighting
     {
 		[Required]
-        public long Id { get; set; }
+        [BsonId]
+        public ObjectId Id { get; set; }
+        [BsonElement("Longitude")]
         public double Longitude { get; set; }
+        [BsonElement("Latitude")]
         public double Latitude { get; set; }
+        [BsonElement("Day")]
         public string Day { get; set; }
         [Required]
+        [BsonElement("SocialHandle")]
         public string SocialHandle { get; set; }
+        [BsonElement("Gender")]
         public string Gender { get; set; }
+        [BsonElement("HairColor")]
         public string HairColor { get; set; }
+        [BsonElement("HairStyle")]
         public string HairStyle { get; set; }
+        [BsonElement("Glasses")]
         public bool Glasses { get; set; }
+        [BsonElement("Message")]
         public string Message { get; set; }
     }
 }
---
+++ backend/Seen/Repositories/SightingRepository.cs
@@ -0,0 +1,34 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using Seen.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Seen.Repositories
+{
+    public class SightingRepository
+    {
+        private IMongoClient client;
+        private IMongoDatabase database;
+        private IMongoCollection<Sighting> sightings;
+
+        public SightingRepository()
+        {
+            client = new MongoClient("mongodb://localhost:27017/");
+            database = client.GetDatabase("Seen");
+            sightings = database.GetCollection<Sighting>("Sightings");
+        }
+
+        public async Task Create(Sighting sighting)
+        {
+            await sightings.InsertOneAsync(sighting);
+        }
+
+        public async Task<List<Sighting>> SelectAll()
+        {
+            return await sightings.Find(new BsonDocument()).ToListAsync();
+        }
+    }
+}
