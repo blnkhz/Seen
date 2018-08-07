b611145bfad390f5e6cce9783a9f52c648017947
Blanka Hooz <hooz.blanka@gmail.com>
2018-08-07 15:43:01 +0200

    add answers.cs model

---
+++ backend/Seen/Models/Answers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Seen.Models
+{
+    public class Answers
+    {
+		public List<string> hairColors = new List<string>()
+        {
+            "Brown", "Blonde", "Black", "Red", "Grey", "Special", "Salt & Pepper"
+        };
+
+        public List<string> hairStyles = new List<string>()
+        {
+            "Bald","Short", "Medium", "Long"
+        };
+
+        public List<string> genders = new List<string>()
+        {
+            "Male", "Female", "Other"
+        };
+    }
+}
