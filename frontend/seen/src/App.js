a8fdac69fc6f775d4966f27f7cbc0376c77a2b17
Peter Nemeth <fbpeter0@gmail.com>
2018-08-07 18:26:41 +0200

    navbar

--- frontend/seen/src/App.js
+++ frontend/seen/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './TWC-logo.png';
 import './App.css';
+import NavbarFeatures from './components/navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavbarFeatures className="navbar" />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Seenek vagyunk</h1>
