58088915d0753306524c9699ef68be7142e64762
Peter Nemeth <fbpeter0@gmail.com>
2018-08-08 15:20:46 +0200

    routing is working

--- frontend/seen/src/App.js
+++ frontend/seen/src/App.js
@@ -3,17 +3,56 @@ import logo from './TWC-logo.png';
 import './App.css';
 import NavbarFeatures from './components/navbar.js';
 import Map from './components/map.js'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom'
+
+const Seen = () =>   
+  <div className="App">
+    <NavbarFeatures className="navbar" />
+    <Map  className="map"/>
+    <header className="App-header">
+      <h1 className="App-title">Seenek vagyunk</h1>
+    </header>
+  </div>
+
+const Have = () => 
+  <div className="App">
+    <NavbarFeatures className="navbar" />
+    <header className="App-header">
+      <h1 className="App-title">Seenek vagyunk</h1>
+    </header>
+  </div>
+
+const routes = [
+  {
+    path: '/seen',
+    component: Seen
+  },
+  {
+    path: '/have',
+    component: Have,
+  }
+]
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Router>
+        <div>
         <NavbarFeatures className="navbar" />
-        <Map  className="map"/>
-        <header className="App-header">
-          <h1 className="App-title">Seenek vagyunk</h1>
-        </header>
-      </div>
+          {routes.map((route) => (
+            <Route
+              key={route.path}
+              path={route.path}
+              component={route.component}
+            />
+          ))}
+        </div>
+      </Router>
+
     );
   }
 }
