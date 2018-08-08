37dd7cb048714fb397801832309d6c6aebdb3313
Peter Nemeth <fbpeter0@gmail.com>
2018-08-08 16:42:24 +0200

    added footer

--- frontend/seen/src/App.js
+++ frontend/seen/src/App.js
@@ -3,6 +3,7 @@ import logo from './TWC-logo.png';
 import './App.css';
 import NavbarFeatures from './components/navbar.js';
 import Map from './components/map.js'
+import FooterPage from './components/footer.js'
 import {
   BrowserRouter as Router,
   Route,
@@ -16,6 +17,7 @@ const Seen = () =>
     <header className="App-header">
       <h1 className="App-title">Seenek vagyunk</h1>
     </header>
+    <FooterPage/>
   </div>
 
 const Have = () => 
