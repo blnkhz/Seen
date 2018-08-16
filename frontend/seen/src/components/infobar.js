40b36389d6e47f7a10d2d731cf19bec2e6d59c7c
Blanka Hooz <hooz.blanka@gmail.com>
2018-08-16 15:58:59 +0200

    delete unnecessary comments :guitar:

--- frontend/seen/src/components/infobar.js
+++ frontend/seen/src/components/infobar.js
@@ -34,32 +34,3 @@ class Infobar extends Component {
 }
 
 export default Infobar;
-
-// const green = '#39D1B4';
-// const yellow = '#FFD712';
-
-// class Toggle extends React.Component {
-//   constructor(props){
-//     super(props);
-//     this.state = { color: green };
-//     this.changeColor = this.changeColor.bind(this);
-//   }
-
-//   changeColor() {
-//     const newColor = this.state.color == green ? yellow : green;
-//     this.setState({ color: newColor });
-//   }
-
-//   render() {
-//     return (
-//       <div style={{background: this.state.color}}>
-//         <h1>
-//           Change my color
-//         </h1>
-//         <button onClick={this.changeColor}>
-//   				Change color
-// 				</button>
-//       </div>
-//     );
-//   }
-// }
