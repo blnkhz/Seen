975da3c1689bf9b541cc8f0763e652878e4d0178
Blanka Hooz <hooz.blanka@gmail.com>
2018-08-22 23:16:19 +0200

    flexin :muscle:

--- frontend/seen/src/components/renderMap.js
+++ frontend/seen/src/components/renderMap.js
@@ -1,33 +1,42 @@
 import React, { Component } from "react";
-import Infos from './infobar.js';
-import Map from './map.js';
+import Infos from "./infobar.js";
+import Map from "./map.js";
 
 class Renderz extends Component {
-    constructor() {
-        super();
-    this.state = { users: []};
-    }
-    
-    componentDidMount = () => {
-      fetch("https://raw.githubusercontent.com/blnkhz/Seen/master/db.json", { mode: "cors" })
+  constructor() {
+    super();
+    this.state = { users: [] };
+  }
+
+  componentDidMount = () => {
+    fetch("https://raw.githubusercontent.com/blnkhz/Seen/master/db.json", {
+      mode: "cors"
+    })
       .then(res => res.json())
       .then(users => this.setState({ users }));
-    };
-  
-    onChild2ButtonClick = (dataFromChild2) => {
-        this.setState({
-          infoIndex: dataFromChild2
-        });
-      };
-    
-    render() {
-      return (
-          <div>
-              <Map onClick={this.onChild2ButtonClick} users = {this.state.users} className="map"></Map>
-              <Infos indexke = {this.state.infoIndex}  userDatas = {this.state.users}/>
-          </div>
-      )
-    }
+  };
+
+  onChild2ButtonClick = dataFromChild2 => {
+    this.setState({
+      infoIndex: dataFromChild2
+    });
+  };
+
+  render() {
+    return (
+      <div className="flexdaddy">
+        <div className="mapchild">
+          <Map onClick={this.onChild2ButtonClick} users={this.state.users} />
+        </div>
+        <div className="infochild">
+          <Infos
+            indexke={this.state.infoIndex}
+            userDatas={this.state.users}
+          />
+        </div>
+      </div>
+    );
   }
-  
-  export default Renderz;
+}
+
+export default Renderz;
