aa6910e36d56b2e32c118baadb7fe13969e72d41
Peter Nemeth <fbpeter0@gmail.com>
2018-08-22 10:46:34 +0200

    nem mennek a mocskos pinek

--- frontend/seen/src/components/map.js
+++ frontend/seen/src/components/map.js
@@ -4,56 +4,66 @@ import InfoBox from "react-google-maps/lib/components/addons/InfoBox";
 import Infos from './infobar.js';
 import maplayout from "./mapstyle.js";
 
+var suti = null;
+var changeClick = (id) => {
+  suti = id;
+}
+
+const GoogleMapExample = withGoogleMap(props => {
+  const kex = props.kex;
+  var sanyi = props.sanyi;
+
+  return(
+  <GoogleMap
+    defaultCenter={{ lat: 47.507589, lng: 19.066128 }}
+    defaultZoom={13}
+    defaultOptions={{
+      streetViewControl: true,
+      scaleControl: false,
+      mapTypeControl: false,
+      panControl: true,
+      zoomControl: true,
+      rotateControl: true,
+      fullscreenControl: true,
+      styles: maplayout
+    }}
+  >
+    {kex.map((element, index) => (
+      <Marker
+        key = {index}
+        icon={require("../assets/seenpinkek.svg")}
+        position={{ lat: element.latitude, lng: element.longitude }}
+        onClick={() => changeClick(index) & console.log(suti)}
+      />
+    ))}
+  </GoogleMap>
+)});
+
 class Map extends Component {
   state = { users: [], clickedIndex: null };
-
+  
   componentDidMount = () => {
     fetch("http://localhost:52210/beenseen", { mode: "cors" })
-      .then(res => res.json())
-      .then(users => this.setState({ users }));
+    .then(res => res.json())
+    .then(users => this.setState({ users }));
   };
-
-  changeClickedIndex(id) {
-    this.setState({
-      clickedIndex: id
-    });
-  }
-
+  
   render() {
-    const GoogleMapExample = withGoogleMap(props => (
-      <GoogleMap
-        defaultCenter={{ lat: 47.507589, lng: 19.066128 }}
-        defaultZoom={13}
-        defaultOptions={{
-          streetViewControl: true,
-          scaleControl: false,
-          mapTypeControl: false,
-          panControl: true,
-          zoomControl: true,
-          rotateControl: true,
-          fullscreenControl: true,
-          styles: maplayout
-        }}
-      >
-        {this.state.users.map((element, index) => (
-          <Marker
-            key = {index}
-            icon={require("../assets/seenpinkek.svg")}
-            position={{ lat: element.latitude, lng: element.longitude }}
-            onClick={() => this.changeClickedIndex(index)}
-          />
-        ))}
-      </GoogleMap>
-    ));
+    console.log(this.state.clickedIndex)
     return (
       <div>
         <GoogleMapExample
-          isMarkerShown
+          kex = {this.state.users}
+          clickedIndex = {suti}
           containerElement={<div className="mapCont" />}
           mapElement={<div className="map" />}
           disableDefaultUI={true}
-        />
-      <Infos indexke = {this.state.clickedIndex} tomb = {this.state.users}/>
+          isMarkerShown
+          
+        >
+        </GoogleMapExample>
+      <Infos indexke = {suti} tomb = {this.state.users}/>
+      {console.log(suti)}
         </div>
     );
   }
