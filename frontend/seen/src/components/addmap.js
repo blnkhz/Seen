cf204758b0f6544c651e96f13678deaaef78e093
Peter Nemeth <fbpeter0@gmail.com>
2018-08-23 15:37:08 +0200

    yesplz

--- frontend/seen/src/components/addmap.js
+++ frontend/seen/src/components/addmap.js
@@ -2,10 +2,11 @@ import React, { Component } from "react";
 import { withGoogleMap, GoogleMap, Marker } from "react-google-maps";
 import maplayout from "./mapstyle.js";
 import update from "react-addons-update";
+import PostForm from "./notstolencode.js"
 
 class AddMap extends Component {
   state = {
-    markers: [{
+    marker: {
       position: {
         lat: null,
         lng: null,
@@ -13,47 +14,46 @@ class AddMap extends Component {
       draggable: true,
       key: "Greenfoxok",
       defaultAnimation: 2
-    }],
-    centerke: { lat: 47.507589, lng: 19.066128 }
+    },
+    centerke: { lat: 47.507589, lng: 19.066128 },
+    savedPos: {lat: null, lng: null}
   }
 
   handleMapClick (event) {
-    var {markers} = this.state;
-    markers = update(markers, {
-      $set: [
+    var {marker} = this.state;
+    marker = update(marker, {
+      $set: 
         {
           position: event.latLng,
           draggable: true,
           defaultAnimation: 2,
-          key: Date.now(),// Add a key property for: http://fb.me/react-warning-keys
+          key: Date.now(),
         },
-      ],
     });
-    this.setState({ markers });
+    this.setState({ marker });
     this.setState({ centerke: event.latLng });
-    console.log(this.state.markers[0].position.lat);
+    console.log(event.latLng.toString());
+    this.setState({savedPos:
+      {
+        lat:parseFloat(parseFloat(event.latLng.toString().substr(1))), 
+        lng:parseFloat(parseFloat(event.latLng.toString().substr(20)))}
+      });
+    console.log(this.state.savedPos);
   }
 
   handleMarkerRightclick () {
-    var {markers} = this.state;
-    markers = update(markers, {
-      $set: [
+    var {marker} = this.state;
+    marker = update(marker, {
+      $set: 
         {
           position: null,
           draggable: true,
           defaultAnimation: 2,
-          key: Date.now(),// Add a key property for: http://fb.me/react-warning-keys
+          key: Date.now(),
         },
-      ],
     });
-    this.setState({ markers });
+    this.setState({ marker });
   }
-
-  shouldComponentUpdate(nextState) {
-    return (this.state.markers !== nextState.markers);
-}
-  
-
   render() {
     const GoogleMapExample = withGoogleMap(props => (
       <GoogleMap
@@ -72,22 +72,18 @@ class AddMap extends Component {
           styles: maplayout
         }}
       >
-     {this.state.markers.map((marker) => {
-              return (
-                <Marker
-                  onRightClick={()=> this.handleMarkerRightclick()}
-                  icon={require("../assets/pin2.svg")}
-                  {...marker}
-                   />
-              );
-            })}
-        ))}
+      <Marker
+        onRightClick={()=> this.handleMarkerRightclick()}
+        icon={require("../assets/pin2.svg")}
+        {...this.state.marker}
+          />
       </GoogleMap>
     ));
-    console.log(this.state.markers);
     return (
-      <div className="mapchild">
+      <div className="flexdaddy">
+        <PostForm savedPos={this.state.savedPos} classname="formchild" />
         <GoogleMapExample
+          savedPos={this.state.savedPos}
           containerElement={<div className="mapCont" />}
           mapElement={<div className="map" />}
         />
