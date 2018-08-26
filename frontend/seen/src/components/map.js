f9ea609397b14a72011144eaca99c1092085e174
Blanka Hooz <hooz.blanka@gmail.com>
2018-08-26 22:36:10 +0200

    add sidebar to Sightings page :eyes:

--- frontend/seen/src/components/map.js
+++ frontend/seen/src/components/map.js
@@ -2,22 +2,25 @@ import React, { Component } from "react";
 import { withGoogleMap, GoogleMap, Marker } from "react-google-maps";
 import maplayout from "./mapstyle.js";
 
-
 class Map extends Component {
-  state = { users: [], zoomka: 13, centerke:{lat:47.507589, lng:19.066128}};
-  
-  onClick(data, latika, longika){
-    this.props.onClick(data);
-    this.setState({centerke:{lat:latika, lng:longika}});
-    this.setState({zoomka: 15});
+  state = {
+    users: [],
+    zoomka: 13,
+    centerke: { lat: 47.507589, lng: 19.066128 }
   };
 
+  onClick(data, latika, longika) {
+    this.props.onClick(data);
+    this.setState({ centerke: { lat: latika, lng: longika } });
+    this.setState({ zoomka: 15 });
+  }
+
   shouldComponentUpdate(nextProps) {
-    return (this.props.users !== nextProps.users);
-}
-  
+    return this.props.users !== nextProps.users;
+  }
+
   render() {
-  const GoogleMapExample = withGoogleMap(props => (
+    const GoogleMapExample = withGoogleMap(props => (
       <GoogleMap
         defaultCenter={{ lat: 47.507589, lng: 19.066128 }}
         defaultZoom={13}
@@ -37,11 +40,13 @@ class Map extends Component {
       >
         {this.props.users.map((element, kulcs) => (
           <Marker
-            key = {kulcs}
+            key={kulcs}
             defaultAnimation="2"
             icon={require("../assets/pin2.svg")}
             position={{ lat: element.latitude, lng: element.longitude }}
-            onClick={() => this.onClick(kulcs, element.latitude, element.longitude)}
+            onClick={() =>
+              this.onClick(kulcs, element.latitude, element.longitude)
+            }
           />
         ))}
       </GoogleMap>
@@ -53,9 +58,9 @@ class Map extends Component {
           mapElement={<div className="map" />}
           disableDefaultUI={true}
           isMarkerShown
-          onClick={this.onClick}>
-        </GoogleMapExample>
-        </div>
+          onClick={this.onClick}
+        />
+      </div>
     );
   }
 }
