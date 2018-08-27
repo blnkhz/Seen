1104eabfe5f1a2404f4d35acec0e17c2f4c12d6a
dugasmate <dugasmate@gmail.com>
2018-08-27 16:59:19 +0200

    add user fields

--- backend/Seen/Models/User.cs
+++ backend/Seen/Models/User.cs
@@ -20,6 +20,14 @@ namespace Seen.Models
         public string UserHairColor { get; set; }
         [BsonElement("UserHairStyle")]
         public string UserHairStyle { get; set; }
+        [BsonElement("UserGlasses")]
+        public string UserGlasses { get; set; }
+        [BsonElement("UserHeight")]
+        public string UserHeight { get; set; }
+        [BsonElement("UserBuild")]
+        public string UserBuild { get; set; }
+        [BsonElement("UserAge")]
+        public string UserAge { get; set; }
         [BsonElement("Sightings")]
         public List<Sighting> Sightings { get; set; }
     }
