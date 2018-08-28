51d4458ee4c8acc3d8402e0bdcbd85897db60e07
dugasmate <dugasmate@gmail.com>
2018-08-28 15:52:12 +0200

    add filter by orientation

--- backend/Seen/Models/Sighting.cs
+++ backend/Seen/Models/Sighting.cs
@@ -6,30 +6,29 @@ using System.Threading.Tasks;
 
 namespace Seen.Models
 {
-        public class Sighting
-        {
-            [BsonElement("Longitude")]
-            public double Longitude { get; set; }
-            [BsonElement("Latitude")]
-            public double Latitude { get; set; }
-            [BsonElement("Day")]
-            public string Day { get; set; }
-            [BsonElement("Gender")]
-            public string Gender { get; set; }
-            [BsonElement("HairColor")]
-            public string HairColor { get; set; }
-            [BsonElement("HairStyle")]
-            public string HairStyle { get; set; }
-            [BsonElement("Glasses")]
-            public string Glasses { get; set; }
-            [BsonElement("Height")]
-            public string Height { get; set; }
-            [BsonElement("Build")]
-            public string Build { get; set; }
-            [BsonElement("Age")]
-            public string Age { get; set; }
-            [BsonElement("Message")]
-            public string Message { get; set; }
-        }
-    
+    public class Sighting
+    {
+        [BsonElement("Longitude")]
+        public double Longitude { get; set; }
+        [BsonElement("Latitude")]
+        public double Latitude { get; set; }
+        [BsonElement("Day")]
+        public string Day { get; set; }
+        [BsonElement("Gender")]
+        public string Gender { get; set; }
+        [BsonElement("HairColor")]
+        public string HairColor { get; set; }
+        [BsonElement("HairStyle")]
+        public string HairStyle { get; set; }
+        [BsonElement("Glasses")]
+        public string Glasses { get; set; }
+        [BsonElement("Height")]
+        public string Height { get; set; }
+        [BsonElement("Build")]
+        public string Build { get; set; }
+        [BsonElement("Age")]
+        public string Age { get; set; }
+        [BsonElement("Message")]
+        public string Message { get; set; }
+    }
 }
--- backend/Seen/Services/UserService.cs
+++ backend/Seen/Services/UserService.cs
@@ -43,21 +43,59 @@ namespace Seen.Services
 
         public async Task<List<User>> Finder(string id)
         {
-            var aLista = await userRepository.SelectAllAsync();
             var aSzemely = await userRepository.SelectByIdAsync(id);
+            List<User> aLista = new List<User>();
+            if (aSzemely.Orientation == "Heterosexual")
+            {
+                string genderValue;
+                genderValue = (aSzemely.UserGender == "Male") ? "Female" : "Male";
+                aLista = await userRepository.SelectByFieldAsync("UserGender", genderValue);
+            }
+
+            if (aSzemely.Orientation == "Homosexual")
+            {
+                string genderValue;
+                genderValue = (aSzemely.UserGender == "Male") ? "Male" : "Female";
+                aLista = await userRepository.SelectByFieldAsync("UserGender", genderValue);
+            }
+
+            if (aSzemely.Orientation == "Bisexual")
+            {
+                aLista = await userRepository.SelectAllAsync();
+            }
+
             var results = new List<User>();
             for (int i = 0; i < aLista.Count; i++)
             {
 
                 for (int j = 0; j < aLista[i].Sightings.Count; j++)
                 {
-                    if (aSzemely.UserGender == aLista[i].Sightings[j].Gender && aSzemely.UserHairColor == aLista[i].Sightings[j].HairColor && aSzemely.UserHairStyle == aLista[i].Sightings[j].HairStyle && aSzemely.Email != aLista[i].Email)
+                    int realmatch = await MatchMeister(aSzemely, aLista[i].Sightings[j]);
+                    if (realmatch == 3 && aSzemely.Email != aLista[i].Email)
                     {
                         results.Add(aLista[i]);
                     }
                 }
             }
             return results;
+        }
+
+        public async Task<int> MatchMeister (User user, Sighting sighting)
+        {
+            int count = 0;
+            if (user.UserHairColor == sighting.HairColor)
+            {
+                count++;
+            }
+            if (user.UserHairStyle == sighting.HairStyle)
+            {
+                count++;
+            }
+            if (user.UserGender == sighting.Gender)
+            {
+                count++;
+            }
+            return count;
         }
     }
 }
