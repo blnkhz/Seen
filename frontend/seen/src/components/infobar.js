4c6d137117e6422baecde7af5f3fe73c9b54b25b
Blanka Hooz <hooz.blanka@gmail.com>
2018-09-06 12:18:05 +0200

    add fields to sightings infobar :feet:

--- frontend/seen/src/components/infobar.js
+++ frontend/seen/src/components/infobar.js
@@ -6,23 +6,24 @@ const Readgend = ({ userDatas, indexke }) => (
       (element, UnIndex) =>
         UnIndex === indexke ? (
           <div className="seenswercontainer">
+            <h4 className="seenpropertytitle">WHEN?</h4>
+            <p className="seenswer">{element.day}</p>
             <h4 className="seenpropertytitle">GENDER</h4>
             <p className="seenswer">{element.gender}</p>
+            <h4 className="seenpropertytitle">AGE</h4>
+            <p className="seenswer">{element.age}</p>
+            <h4 className="seenpropertytitle">BUILD</h4>
+            <p className="seenswer">{element.build}</p>
+            <h4 className="seenpropertytitle">HEIGHT</h4>
+            <p className="seenswer">{element.height}</p>
             <h4 className="seenpropertytitle">HAIR</h4>
             <p className="seenswer">
               {element.hairColor}, {element.hairStyle}
             </p>
             <h4 className="seenpropertytitle">MESSAGE</h4>
             <p className="seenswer">{element.message}</p>
-            <h4 className="seenpropertytitle">WHEN?</h4>
-            <p className="seenswer">{element.day}</p>
-            <h4 className="seenpropertytitle">HANDLE</h4>
-            <p className="seenswer">{element.socialHandle}</p>
             <a href="/itsamatch">
-              <button
-                type="submit"
-                className="it-is-me-button"
-              >
+              <button type="submit" className="it-is-me-button">
                 HELLO IT'S ME
               </button>
             </a>
