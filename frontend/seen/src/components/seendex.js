5f121bb13b954ad729e681161932360952b79653
Blanka Hooz <hooz.blanka@gmail.com>
2018-09-12 15:56:50 +0200

    (style) make text appear on hover :snake:

--- frontend/seen/src/components/seendex.js
+++ frontend/seen/src/components/seendex.js
@@ -11,38 +11,46 @@ const ezleszenittenazigaziindex = (
       />
     </NavLink>
     <div id="seendex-container">
-      <NavLink to="/add">
-        <img
-          src={require("../assets/plusz.png")}
-          className="index-icon add-logo"
-          alt="add"
-        />
-      </NavLink>
-      <p className="icon-text">HAVE SEEN</p>
-      <NavLink to="/sightings">
-        <img
-          src={require("../assets/nagyito2.png")}
-          className="index-icon sightings-logo"
-          alt="sightings"
-        />
-      </NavLink>
-      <p className="icon-text">BEEN SEEN?</p>
-      <NavLink to="/faq">
-        <img
-          src={require("../assets/pinkquestion.png")}
-          className="index-icon faq-logo"
-          alt="faq"
-        />
-      </NavLink>
-      <p className="icon-text">FAQ</p>
-      <NavLink to="/about">
-        <img
-          src={require("../assets/mink.png")}
-          className="index-icon about-logo"
-          alt="about"
-        />
-      </NavLink>
-      <p className="icon-text">ABOUT</p>
+      <div className="seendex-icon">
+        <NavLink to="/add">
+          <img
+            src={require("../assets/plusz.png")}
+            className="index-icon add-logo"
+            alt="add"
+          />
+        </NavLink>
+        <p className="icon-text">HAVE SEEN</p>
+      </div>
+      <div className="seendex-icon">
+        <NavLink to="/sightings">
+          <img
+            src={require("../assets/nagyito2.png")}
+            className="index-icon sightings-logo"
+            alt="sightings"
+          />
+        </NavLink>
+        <p className="icon-text">BEEN SEEN?</p>
+      </div>
+      <div className="seendex-icon">
+        <NavLink to="/faq">
+          <img
+            src={require("../assets/pinkquestion.png")}
+            className="index-icon faq-logo"
+            alt="faq"
+          />
+        </NavLink>
+        <p className="icon-text">FAQ</p>
+      </div>
+      <div className="seendex-icon">
+        <NavLink to="/about">
+          <img
+            src={require("../assets/mink.png")}
+            className="index-icon about-logo"
+            alt="about"
+          />
+        </NavLink>
+        <p className="icon-text">ABOUT</p>
+      </div>
     </div>
   </div>
 );
