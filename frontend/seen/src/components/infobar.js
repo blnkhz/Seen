9b29eefb00debbc383386aaceafc68676fc59d6d
Peter Nemeth <fbpeter0@gmail.com>
2018-09-19 17:54:46 +0200

    adding infoboxes, tooltips and map refactor, not ready yet

--- frontend/seen/src/components/infobar.js
+++ frontend/seen/src/components/infobar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactTooltip from 'react-tooltip'
 
 const Readgend = ({ socialHandle, userDatas, indexke }) => (
   <div>
@@ -7,13 +8,16 @@ const Readgend = ({ socialHandle, userDatas, indexke }) => (
         UnIndex === indexke ? (
           console.log(element.id),
           <div className="seenswercontainer">
-          <h4 className="infotitle">looks familiar?</h4>
-            <img className="infobar-picture" src={element.picture} alt="hmmm"></img>
+            <h4 className="infotitle">looks familiar?</h4>
+            <img data-tip data-for='sajt' className="infobar-picture" src={element.picture} alt="hmmm"></img>
             <h4 className="seenpropertytitle">WHEN?</h4>
             <p className="seenswer">{element.day}</p>
             <h4 className="seenpropertytitle">MESSAGE</h4>
             <p className="seenswer">{element.message}</p>
             <a href="/itsamatch">
+            <ReactTooltip id='sajt' type='light' effect='float'>
+              <span>This person is looking for someone like you!</span>
+            </ReactTooltip>
               <button type="submit" className="it-is-me-button">
                 YES!
               </button>
--- frontend/seen/src/components/map.js
+++ frontend/seen/src/components/map.js
@@ -1,56 +1,78 @@
 import React, { Component } from "react";
-import { withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+import { withGoogleMap, GoogleMap, Marker, InfoWindow } from "react-google-maps";
 import maplayout from "./mapstyle.js";
+
+const GoogleMapExample = withGoogleMap(props => (
+  <GoogleMap
+    defaultCenter={{ lat: 47.507589, lng: 19.066128 }}
+    defaultZoom={13}
+    center={props.centerke}
+    zoom={props.zoomka}
+    defaultOptions={{
+      streetViewControl: true,
+      scaleControl: false,
+      mapTypeControl: false,
+      panControl: true,
+      zoomControl: true,
+      rotateControl: true,
+      fullscreenControl: true,
+      minZoom: 3,
+      styles: maplayout
+    }}
+  >
+    {props.users.map((element, kulcs) => (
+      <div>
+        <Marker
+          key={kulcs}
+          defaultAnimation="2"
+          icon={require("../assets/pin2.svg")}
+          onMouseOver={()=>props.mouseOver() && props.boxPos(kulcs, element.latitude, element.longitude)} 
+          onMouseOut={props.mouseExit}
+          position={{ lat: element.latitude, lng: element.longitude }}
+          onClick={() =>
+            props.setKeksz(kulcs)
+          }
+        >
+          {props.showInfoWindow && (<InfoWindow position={props.boxPosValue}>
+            <span>krumpli</span>
+          </InfoWindow>)}
+        </Marker>
+      </div>
+    ))}
+  </GoogleMap>
+));
 
 class Map extends Component {
   state = {
     users: [],
     zoomka: 13,
-    centerke: { lat: 47.507589, lng: 19.066128 }
+    centerke: { lat: 47.507589, lng: 19.066128 },
+    showInfoWindow: false,
+    indkeksz: 0,
+    ind: 0
   };
 
-  onClick(data, latika, longika) {
-    this.props.onClick(data);
-    this.setState({ centerke: { lat: latika, lng: longika } });
-    this.setState({ zoomka: 15 });
+  handleMouseOver = e => {
+    this.setState({
+      showInfoWindow: true
+    });
+  };
+  handleMouseExit = e => {
+    this.setState({
+      showInfoWindow: false
+    });
+  };
+
+  getBoxPos(data, lato, lngo) {
+    this.setState({centerke: {lat:lato, lng:lngo}, ind: data})
   }
 
-  shouldComponentUpdate(nextProps) {
-    return this.props.users !== nextProps.users;
+  setIndkeksz = (data) =>{
+    this.setState({indkeksz: data});
+    this.props.setKeksz(data);
   }
 
   render() {
-    const GoogleMapExample = withGoogleMap(props => (
-      <GoogleMap
-        defaultCenter={{ lat: 47.507589, lng: 19.066128 }}
-        defaultZoom={13}
-        center={this.state.centerke}
-        zoom={this.state.zoomka}
-        defaultOptions={{
-          streetViewControl: true,
-          scaleControl: false,
-          mapTypeControl: false,
-          panControl: true,
-          zoomControl: true,
-          rotateControl: true,
-          fullscreenControl: true,
-          minZoom: 3,
-          styles: maplayout
-        }}
-      >
-        {this.props.users.map((element, kulcs) => (
-          <Marker
-            key={kulcs}
-            defaultAnimation="2"
-            icon={require("../assets/pin2.svg")}
-            position={{ lat: element.latitude, lng: element.longitude }}
-            onClick={() =>
-              this.onClick(kulcs, element.latitude, element.longitude)
-            }
-          />
-        ))}
-      </GoogleMap>
-    ));
     return (
       <div>
         <GoogleMapExample
@@ -58,7 +80,18 @@ class Map extends Component {
           mapElement={<div className="map" />}
           disableDefaultUI={true}
           isMarkerShown
-          onClick={this.onClick}
+          onClick={this.setIndkeksz}
+          centerke={this.state.centerke}
+          zoomka={this.state.zoomka}
+          users={this.props.users}
+          showInfoWindow={this.state.showInfoWindow}
+          mouseOver={this.handleMouseOver}
+          mouseExit={this.handleMouseExit}
+          click={this.handleClick}
+          setKeksz={this.setIndkeksz}
+          indo={this.state.indkeksz}
+          boxPos={this.getBoxPos}
+          boxPosValue={this.state.centerke}
         />
       </div>
     );
