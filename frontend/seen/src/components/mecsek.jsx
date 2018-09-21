10ab405c87a56cecfce6605f733657acbc81afc0
Blanka Hooz <hooz.blanka@gmail.com>
2018-09-21 15:18:05 +0200

    add sightings to sightings page (no styling so far)

--- frontend/seen/src/components/mecsek.jsx
+++ frontend/seen/src/components/mecsek.jsx
@@ -1,28 +1,42 @@
 import React, { Component } from "react";
 import { NavLink } from "mdbreact";
 
-
 class Mecsek extends Component {
   constructor() {
     super();
-  this.state = {
-    fbuser: null
-  };
-}
-  componentWillMount = () => { 
+    this.state = {
+      fbuser: null
+    };
+  }
+  componentWillMount = () => {
     fetch("http://localhost:52210/getuser/" + this.props.id, {
       mode: "cors"
     })
       .then(res => res.json())
-      .then(fbuser => {this.setState({ fbuser })});
+      .then(fbuser => {
+        this.setState({ fbuser });
+      });
   };
-  render(
-  ) {
+  render() {
     if (this.props.id === null || this.state.fbuser === null) {
-      return <div>SANYIKAM!</div>;}
-    const nyanyi =
-      <h1>{console.log(this.state.fbuser)}</h1>;
-    return nyanyi;
+      return <h1 style={{ margin: "200px" }}>SANYIKAM!</h1>;
+    }
+    return (
+      <div id="match-container">
+        {this.state.fbuser.sightings.map(element => (
+          <div className="meccsiz">
+            <h1>{element.day}</h1>
+            {element.helloItsMes.map(hello => (
+              <div className="sighting-applicants">
+                <h4>{hello.socialHandle}</h4>
+                <img src={hello.picture} />
+                <p>{hello.message}</p>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    );
   }
 }
 
