3338e7048d547ec37eedf75cacb98e79c935da29
dugasmate <dugasmate@gmail.com>
2018-09-21 14:54:55 +0200

    cute complimentz

--- frontend/seen/src/components/infobar.js
+++ frontend/seen/src/components/infobar.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactTooltip from 'react-tooltip'
 
-const niceThings = ['You look good', 'What a beauty', 'You smell nice']
+const niceThings = ["You're a gift to those around you.", "You look good!", "What a beauty!", "You smell nice.", "You're a smart cookie.", "You are awesome!", "I like your style.", "You have the best laugh.", "You light up the room.", "You deserve a hug right now.", "You're all that and a super-size bag of chips.", "On a scale from 1 to 10, you're an 11.",
+"You're like a ray of sunshine on a really dreary day.", "Everything would be better if more people were like you!", "Colors seem brighter when you're around.", "You're wonderful.", 
+"You have cute elbows. For reals!", "You're someone's reason to smile.", "There's ordinary, and then there's you."]
 
 class Readgend extends React.Component{
   constructor() {
